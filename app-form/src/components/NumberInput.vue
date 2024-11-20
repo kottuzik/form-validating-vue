e36eeb75e8ea0660ae31985be25cb743c18ac628
evas <evas@classe.world>
2024-11-20 14:36:13 +0200

    feat: second commit

---
+++ app-form/src/components/NumberInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <label :for="field.id">{{ field.label }}</label>
+    <input
+        :id="field.id"
+        type="number"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+        @blur="validate"
+    />
+    <span v-if="errors" class="error">
+      {{ errors }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NumberInput',
+  props: {
+    field: { type: Object, required: true },
+    modelValue: { type: [String, Number], required: true },
+    errors: { type: String, required: false },
+  },
+  methods: {
+    validate() {
+      this.$emit('validate', this.field.id, this.modelValue);
+    },
+  },
+};
+</script>
