7b01b1a2fb52327242bee4d34d094f93e4c6ce9f
evas <evas@classe.world>
2024-12-04 11:09:20 +0200

    feat: first steps with router

---
+++ app/src/components/InnerPages/AboutMe.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>About Me page</h2>
+</template>
+
+<script>
+export default {
+  name: "AboutMe"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/src/components/InnerPages/AnotherPage.vue
@@ -0,0 +1,13 @@
+<template>
+<h2>Another page</h2>
+</template>
+
+<script>
+export default {
+  name: "AnotherPage"
+}
+</script>
+
+<style scoped>
+  h2{font-size: 32px}
+</style>
